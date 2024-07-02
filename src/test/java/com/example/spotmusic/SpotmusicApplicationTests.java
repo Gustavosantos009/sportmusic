473ea8cb348e5fd93262fa23de338b716dfad46b
Gustavo Santos <gustavosantos009@gmail.com>
2024-07-01 22:50:40 -0300

    Inicio projeto

---
+++ src/test/java/com/example/spotmusic/SpotmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
